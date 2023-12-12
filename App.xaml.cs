34d508619e83e4aa67f879403a14a5911227c1e2
Ilya Trufakin <trufakin@hotmail.com>
2023-12-12 23:05:29 +0200

    Ver-0.01

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Rock_paper_scissors_Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
